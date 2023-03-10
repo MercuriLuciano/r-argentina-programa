019e98b983349b69daec283be44c78a534b87654
Luciano Mercuri <lucianomercuri86@gmail.com>
2023-03-10 00:38:52 -0300

    Assignment 1: Minor fixes and functions added

--- class-6/assignment-1/src/functions.js
+++ class-6/assignment-1/src/functions.js
@@ -1,3 +1,10 @@
+function deletePreviousRelatives($ageForm, $result){
+    $ageForm.innerHTML = '';
+    for(let i = 0; i < $result.length; i++){
+        $result[i].textContent = '';
+    }
+}
+
 function createInput(parent){
     const $input = document.createElement('input');
     $input.type = 'number';
@@ -13,9 +20,11 @@ function createLabel(familyMemberNumber, parent){
     parent.appendChild($label);
 }
 
-function addFamilyMember(familyMemberNumber, parent){
-    createLabel(familyMemberNumber, parent);
-    createInput(parent);
+function addFamilyMembers(parent, amountOfPeople){
+    for(let i = 0; i < amountOfPeople; i++){
+        createLabel(i, parent);
+        createInput(parent);
+    }
 }
 
 function createButton(id, text, parent){
@@ -43,7 +52,7 @@ function calculateAverageAge(sumOfAges, amountOfPeople){
     return sumOfAges / amountOfPeople;
 }
 
-function checkInputs($familyMemberAgeInputs){
+function checkInputs($familyMemberAgeInputs, invalidInput){
     if($familyMemberAgeInputs.value === '' || $familyMemberAgeInputs.value < 0 || $familyMemberAgeInputs.value > 120){
         $familyMemberAgeInputs.style.borderColor = '#f00';
         invalidInput = true;
@@ -51,4 +60,27 @@ function checkInputs($familyMemberAgeInputs){
     else{
         $familyMemberAgeInputs.style.borderColor = '';
     }
+    return invalidInput;
+}
+
+function displayResult(max, min, sumOfAges, amountOfPeople){
+    document.querySelector('#maximum-age').textContent = `The oldest family member is ${max} years old`;
+    document.querySelector('#minimum-age').textContent = `The youngest family member is ${min} years old`;
+    document.querySelector('#average-age').textContent = `The average age is ${calculateAverageAge(sumOfAges, amountOfPeople)} years`;
+}
+
+function reset($ageForm, $result){
+    deletePreviousRelatives($ageForm, $result);
+    document.querySelector('#container').style.background = '';
+    document.querySelector('#amount-of-people').value = '';
+}
+
+function validInputStyles(){
+    document.querySelector('#container').style.background = '#5b775e';
+    document.querySelector('#amount-of-people').style.borderColor = '';
+}
+
+function invalidInputStyles(){
+    document.querySelector('#container').style.background = '';
+    document.querySelector('#amount-of-people').style.borderColor = '#f00';
 }
--- class-6/assignment-1/src/index.js
+++ class-6/assignment-1/src/index.js
@@ -1,30 +1,20 @@
-const $submitButton = document.querySelector('#submit-button');
-
-$submitButton.onclick = function(){
+document.querySelector('#submit-button').onclick = function(){
     const amountOfPeople = document.querySelector('#amount-of-people').value;
     const $ageForm = document.querySelector('#age-form');
     const $result = document.querySelectorAll('span');
 
-    $ageForm.innerHTML = '';        // Deletes the content that was created with the submit and calculate buttons in case the user changes the number of relatives without using the clear button
-    for(let i = 0; i < $result.length; i++){
-        $result[i].textContent = '';
-    }
-    
+    deletePreviousRelatives($ageForm, $result);
 
     if(amountOfPeople !== '' && amountOfPeople > 0 && amountOfPeople < 25){
 
-        document.querySelector('#container').style.background = '#5b775e';
-    
-        for(let i = 0; i < amountOfPeople; i++){
-            addFamilyMember(i, $ageForm);
-        }
+        validInputStyles();
+        addFamilyMembers($ageForm, amountOfPeople);
     
         if(!document.querySelector('#calculate-button')){
-
+            
             createButton('calculate-button', 'Calculate', $ageForm);
-            const $calculateButton = document.querySelector('#calculate-button');
 
-            $calculateButton.onclick = function(){
+            document.querySelector('#calculate-button').onclick = function(){
 
                 const $familyMemberAgeInputs = document.querySelectorAll('.family-member-age');
                 let max = -9999;
@@ -32,45 +22,31 @@ $submitButton.onclick = function(){
                 let sumOfAges = 0;
                 let invalidInput = false;
 
-
                 for(let i = 0; i < amountOfPeople; i++){
                     const inputIValue = Number($familyMemberAgeInputs[i].value);
                     sumOfAges += inputIValue;
                     max = calculateMaxAge(max, inputIValue);
                     min = calculateMinAge(min, inputIValue);
-                    checkInputs($familyMemberAgeInputs[i]);
+                    invalidInput = checkInputs($familyMemberAgeInputs[i], invalidInput);
                 }
 
                 if(!invalidInput){
-
-                    document.querySelector('#maximum-age').textContent = `The oldest family member is ${max} years old`;
-                    document.querySelector('#minimum-age').textContent = `The youngest family member is ${min} years old`;
-                    document.querySelector('#average-age').textContent = `The average age is ${calculateAverageAge(sumOfAges, amountOfPeople)} years`;
+                    displayResult(max, min, sumOfAges, amountOfPeople);
                 }
 
                 return false;
             }
-
 
             createButton('clear-button', 'Clear', $ageForm);
-            const $clearButton = document.querySelector('#clear-button');
 
-            $clearButton.onclick = function(){      // Clear button, delete the content that was created with the submit and calculate buttons
-
-                $ageForm.innerHTML = '';
-                for(let i = 0; i < $result.length; i++){
-                    $result[i].textContent = '';
-                }
-                document.querySelector('#container').style.background = '';
-                document.querySelector('#amount-of-people').value = '';
-
+            document.querySelector('#clear-button').onclick =  function(){     // Clear button, delete the content that was created with the submit and calculate buttons
+                reset($ageForm, $result);
                 return false;
-            }
-
+            }   
         }
     }
     else{
-        document.querySelector('#amount-of-people').style.borderColor = '#f00';
+        invalidInputStyles();
     }
 
     return false;
